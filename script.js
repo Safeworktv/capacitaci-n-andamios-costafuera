82f25d1fd1be315431ffbe3327704bff9b126fcb
Safeworktv <eakmx79@gmail.com>
2026-04-23 08:06:54 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function mostrarGlosario() {
+    alert("Cargando Glosario de Componentes: Postes, Largueros, Charolas y Seguros...");
+    // Aquí programaremos la galería interactiva después
+}
+
+function iniciarJuego() {
+    document.getElementById('pantalla-inicio').style.display = 'none';
+    document.getElementById('zona-inspeccion').style.display = 'block';
+    alert("Iniciando Fase 1: Inspección de Subnivel Costafuera");
+}
